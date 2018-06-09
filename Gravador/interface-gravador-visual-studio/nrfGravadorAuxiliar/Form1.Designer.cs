aa9d302bb2f62976112771b2049e226d052c8db8
Ítalo Fernandes <italogsfernandes@gmail.com>
2018-06-09 13:33:11 -0300

    gravador visual studio

---
+++ Gravador/interface-gravador-visual-studio/nrfGravadorAuxiliar/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace nrfGravadorAuxiliar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 368);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(922, 23);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "nrf Auxiliar de Gravação";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(149, 344);
+            this.panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Opções:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Abrir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 171);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(149, 173);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "--------\n";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(922, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.richTextBox2);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(149, 24);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(773, 344);
+            this.panel3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Resultado:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(0, 13);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(773, 331);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 391);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Programa Auxiliar de Gravação do nRF24LE1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Gravador/interface-gravador-visual-studio/nrfGravadorAuxiliar/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nrfGravadorAuxiliar
+{
+    public partial class Form1 : Form
+    {
+        public program_data codigo_atual;
+        public program_data codigo_lido;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Auxiliar de Gravador do nrf24le1";
+            user_log("Aplicação Iniciada.");
+            openFileDialog1.Title = "Selecione o arquivo .hex gerado pelo Keil.";
+            codigo_atual = new program_data();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            user_log("Abrindo Arquivo");
+            openFileDialog1.ShowDialog();
+        }
+
+
+        public void user_log(string mensagem)
+        {
+            richTextBox1.Text = "Status: " + "\n" + System.DateTime.Now.TimeOfDay.ToString().Substring(0, 8) + " - "
+                + mensagem + richTextBox1.Text.Substring(8);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {          
+            user_log("Arquivo selecionado: " + openFileDialog1.FileName.ToString());
+            // Open the file for reading.
+            System.IO.StreamReader file =
+                System.IO.File.OpenText(openFileDialog1.FileName);
+
+            string str_codigo_hex = file.ReadToEnd();
+
+            codigo_atual.carregar_arquivo(str_codigo_hex);
+
+            user_log("Lido:\n" + str_codigo_hex.Substring(0,12) + "\n[...]\n" + str_codigo_hex.Substring(str_codigo_hex.Length - 12,12));
+            user_log("Arquivo lido com sucesso.");
+
+            richTextBox2.Clear();
+
+            export_vector(codigo_atual);
+            //show_code(codigo_atual);
+            // Set the MainWindow Title to the filename.
+            this.Text = "Auxiliar de Gravador do nrf24le1 -- " + openFileDialog1.FileName.ToString();
+            file.Close(); // Close the file so as to not leave a mess.
+
+        }
+
+        private void export_vector(program_data codigo)
+        {
+            string str_out = "***********************************************************************************\n" +
+                        "Copie o seguinte código em seu programa em C: \n\n" +
+                        "uint8_t hex_code[" + codigo.hex_code.Length.ToString() + "] = {\n";
+            for (int i = 0; i < Math.Ceiling((float)(codigo.hex_code.Length) / 16); i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    if (codigo.hex_code[i * 16 + j] < 16)
+                    {
+                        str_out += "0x0" + codigo.hex_code[i * 16 + j].ToString("X");
+                    }
+                    else
+                    {
+                        str_out += "0x" + codigo.hex_code[i * 16 + j].ToString("X");
+                    }
+                    if (j == 15 && i == Math.Ceiling((float)(codigo.hex_code.Length) / 16) - 1)
+                    {
+                        str_out += " };";
+                    }
+                    else
+                    {
+                        str_out += ", ";
+                    }
+                }
+                str_out += "\n";
+            }
+            str_out += "\n***********************************************************************************\n";
+            richTextBox2.Text = str_out;
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text =
+    "Software desenvolvido para realizar o upload de codigos" +
+    "\n do tipo .hex para o transponder nrf24le1." +
+    "\n Este codigo é apenas uma versão simplificada contendo"+
+    "\n apenas a funcionalidade de ler um arquivo .hex e gerar um" +
+    "\n array em linguagem c contendo as informações deste arquivo." +
+    "\n" +
+    "\n No caso do PIC este vetor pode ser copiado e colocado no"+
+    "\n código dele." +
+    "\n" +
+    "\n Foi desenvolvido como parte de um programa que," +
+    "\n junto ao arduino due devidademente configurado," +
+    "\n é capaz de gravar códigos em CIs nrf24le1." +
+    "\n Para ativar esta opção utilize a versão em Gtk-sharp (nrf-Burner)." +
+    "\n link: https://github.com/italogfernandes/ltad-propinsitu/tree/master/GUI%20-%20Gravador%20do%20nrf" +
+    "\n" +
+    "\n Desenvolvido por Italo Fernandes em 2016\n" +
+    "\n GitHub:  https://github.com/italogfernandes" +
+    "\n Contato: italogsfernandes@gmail.com";
+        }
+    }
+}
