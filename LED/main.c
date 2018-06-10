92df3fe7c769d53beacaff493b7ef3f0346643ee
TallesSilva <tallesbugs@hotmail.com>
2018-06-10 11:16:23 -0300

    adicionei uns elementos, espero não desatualizar o site.

--- LED/main.c
+++ LED/main.c
@@ -1,8 +1,21 @@
 #include<Nordic\reg24le1.h>
+#incude<hal_delay.h>
 
+void setup(){
 void main(){
-	P0DIR = 0x00;
-	while(1){
-		P03 = !P03;
+	P0DIR = 0xff;//entrada
+	P1DIR = 0xff;//entrada
+	P0DIR = 0xef //setar bit4 do p0dir para 0
 	}
 }
+void loop(){	
+		P04 = 1;
+		delay_ms(1000);
+		P04 = 0;
+		delay_ms(1000);
+}
+
+
+	while(1){
+		loop()
+	}
